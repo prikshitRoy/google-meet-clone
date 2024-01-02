94bf4f7a8a1d06aaf8433e4a10e57f4f9cf397c0
parikshitRoy <parikshitroy00@gmail.com>
2024-01-02 12:37:14 +0530

    Created UI, buttom & its logic

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,39 @@
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/navigation";
+
+import styles from "@/styles/home.module.css";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>Welcome</div>;
+  const router = useRouter();
+  const [roomId, setRoomID] = useState("");
+
+  const createAndJoin = () => {
+    const roomId = uuidv4();
+    router.push(`/${roomId}`);
+  };
+
+  const joinRoom = () => {
+    if (roomId) router.push(`/${roomId}`);
+    else {
+      alert("Please provide a valid room ID");
+    }
+  };
+  return (
+    <div className={styles.homeContainer}>
+      <h1>Google Meet Clone</h1>
+      <div className={styles.enterRoom}>
+        <input
+          placeholder="Enter RoomId"
+          value={roomId}
+          onChange={(e) => setRoomID(e?.target?.value)}
+        />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      <span className={styles.separatorText}>
+        -------------- OR -------------------
+      </span>
+      <button onClick={createAndJoin}>Create a new Room</button>
+    </div>
+  );
 }
